b11660f0945ee7cea54fb3fd446d6eeef0880472
Tonrkz <tonrak.5@hotmail.com>
2024-01-12 00:22:46 +0700

    MovingPlayer

---
+++ Assets/Scripts/MovingPlayer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovingPlayer : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler, IEndDragHandler {
+
+    bool isDrag = false;
+    [SerializeField] float maxMoveSpeed = 10;
+    [SerializeField] float smoothTime = 0.3f;
+    Vector2 currentVelocity;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        if (isDrag) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("Clicked");
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        Debug.Log("UnClicked");
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        isDrag = true;
+        Debug.Log("Dragged");
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        isDrag = false;
+        Debug.Log("Undragged");
+    }
+}

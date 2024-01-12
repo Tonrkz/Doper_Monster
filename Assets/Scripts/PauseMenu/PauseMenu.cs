384299e415e82160aa741a2fcfe50f2fe0ad95f3
Tonrkz <tonrak.5@hotmail.com>
2024-01-12 23:20:19 +0700

    Pause Menu

---
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField] GameObject pauseMenuObject;
+    [SerializeField] GameObject pauseButton;
+
+    private void Start() {
+        pauseMenuObject.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    public void PauseGame() {
+        pauseButton.SetActive(false);
+        pauseMenuObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame() {
+        pauseButton.SetActive(true);
+        pauseMenuObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToMainMenu() {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+}

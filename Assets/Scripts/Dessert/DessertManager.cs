9477ee809a61c2722cc0a09e323c9724d78aef8b
Tonrkz <tonrak.5@hotmail.com>
2024-03-16 20:58:55 +0700

    Dessert Sprite Implementation

--- Assets/Scripts/Dessert/DessertManager.cs
+++ Assets/Scripts/Dessert/DessertManager.cs
@@ -19,37 +19,40 @@ public class DessertManager : MonoBehaviour {
     [SerializeField] DessertType type;
     public DessertType Type { get { return type; } set { type = value; } }
 
+    [SerializeField] List<Sprite> dessertSpriteList = new List<Sprite>();
+
     public void SetupDessert(DessertType type, int basePoint) {
         SpriteRenderer rdr = GetComponent<SpriteRenderer>();
         switch (type) {
             case DessertType.lowPoint:
-                rdr.color = new Color32(184, 255, 186, 255);
+                rdr.sprite = dessertSpriteList[0];
                 point = 3 + basePoint;
                 break;
             case DessertType.midPoint:
-                rdr.color = new Color32(117, 255, 121, 255);
+                rdr.sprite = dessertSpriteList[1];
                 point = 7 + basePoint;
                 break;
             case DessertType.highPoint:
-                rdr.color = new Color32(43, 255, 49, 255);
+                rdr.sprite = dessertSpriteList[2];
                 point = 13 + basePoint;
                 break;
             case DessertType.heal:
-                rdr.color = new Color32(255, 118, 99, 255);
+                rdr.sprite = dessertSpriteList[3];
                 point = 5;
                 break;
             case DessertType.damage:
-                rdr.color = new Color32(85, 27, 171, 255);
+                rdr.sprite = dessertSpriteList[4];
                 point = 0;
                 break;
             case DessertType.speed:
-                rdr.color = new Color32(255, 255, 61, 255);
+                rdr.sprite = dessertSpriteList[5];
                 point = 2;
                 break;
             case DessertType.invis:
-                rdr.color = new Color32(0, 229, 255, 255);
+                rdr.sprite = dessertSpriteList[6];
                 point = 4;
                 break;
         }
+        gameObject.AddComponent<PolygonCollider2D>();
     }
 }
